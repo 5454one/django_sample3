7445a0d216ded7be6f87c75aa649e0dc8f7d3c62
5454/one <kmm2459379@stu.o-hara.ac.jp>
2025-10-17 02:10:51 +0000

    gtt

--- app/polls/admin.py
+++ app/polls/admin.py
@@ -7,11 +7,11 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
-    ]
-    
+    list_display = [
+        "question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
